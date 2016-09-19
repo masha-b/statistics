01c10d26c6f631dcf3f314ac56099e52ea7961db
maria <mariabiryukova@mbp-maria.dlink>
2016-09-19 19:27:30 +0500

    Initial Commit

---
+++ iStatistics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iStatistics
+//
+//  Created by Maria Biryukova on 19.09.16.
+//  Copyright © 2016 Maria Biryukova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
