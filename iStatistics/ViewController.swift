605394a3983d0414106fc7ab1e3b8c89811550b7
Maria Biryukova <mariabiryukova@MBP-Maria.Dlink>
2016-09-19 19:59:20 +0500

    rename project

--- iStatistics/ViewController.swift
+++ iStatistics/ViewController.swift
@@ -1,9 +1,9 @@
 //
 //  ViewController.swift
-//  iStatistics
+//  test
 //
-//  Created by Maria Biryukova on 19.09.16.
-//  Copyright © 2016 Maria Biryukova. All rights reserved.
+//  Created by Anna Lazareva on 16/09/16.
+//  Copyright © 2016 Anna Lazareva. All rights reserved.
 //
 
 import UIKit
@@ -12,12 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        loadTestData()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadTestData() {
+        if let path = Bundle.main.path(forResource: "testData", ofType: "json") {
+            do {
+                let jsonData = try NSData(contentsOfFile: path, options: NSData.ReadingOptions.mappedIfSafe)
+                do {
+                    let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                    
+                    if let statistics : [NSDictionary] = jsonResult["statistics"] as? [NSDictionary] {
+                        for stat: NSDictionary in statistics {
+                            print("\(stat)")
+                        }
+                    }
+                } catch {}
+            } catch {}
+        }
     }
 
 
