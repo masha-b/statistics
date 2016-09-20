6eadc2d11f17a290a603820c8eb00cfc4d6dfdeb
Anna Lazareva <annalazareva@ip-192-168-10-55.us-west-2.compute.internal>
2016-09-20 18:44:43 +0500

    restruct, add migration

--- iStatistics/Controller/CoreDataStack.swift
+++ iStatistics/Controller/CoreDataStack.swift
@@ -8,6 +8,8 @@ class CoreDataStack {
     
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "iStatistics")
+        let migrateOptions = [NSMigratePersistentStoresAutomaticallyOption: true,
+                        NSInferMappingModelAutomaticallyOption: true]
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
